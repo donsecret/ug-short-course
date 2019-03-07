fb609b89cca5ef1f749cbb70ebaadf9121960142
Dennis Kwabena Bilson Jr <41457025+Quabynah-Codelabs@users.noreply.github.com>
2019-03-07 13:30:39 +0000

    Added dependencies

---
+++ android-applications/church-inc/app/src/main/java/io/codelabs/churchinc/core/RootActivity.kt
@@ -0,0 +1,10 @@
+package io.codelabs.churchinc.core
+
+/**
+ * Root activity for all Activities in this application
+ */
+abstract class RootActivity {
+
+
+
+}
---
+++ android-applications/church-inc/app/src/main/java/io/codelabs/churchinc/view/SplashActivity.kt
@@ -0,0 +1,6 @@
+package io.codelabs.churchinc.view
+
+import io.codelabs.churchinc.core.RootActivity
+
+class SplashActivity : RootActivity() {
+}
