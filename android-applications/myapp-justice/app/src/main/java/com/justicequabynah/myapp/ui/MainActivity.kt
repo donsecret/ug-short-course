131e28519fcb4a96ba691c45db45860f0212dc2d
Dennis Kwabena Bilson Jr <41457025+Quabynah-Codelabs@users.noreply.github.com>
2019-06-14 12:41:57 +0000

    Added new android studio project

---
+++ android-applications/myapp-justice/app/src/main/java/com/justicequabynah/myapp/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.justicequabynah.myapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.justicequabynah.myapp.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
