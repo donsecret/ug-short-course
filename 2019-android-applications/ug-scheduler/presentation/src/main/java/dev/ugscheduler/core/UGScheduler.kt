ae73389456168acbc0959e0ca4b003f2fbc0a570
Dennis Kwabena Bilson Jr <41457025+Quabynah-Codelabs@users.noreply.github.com>
2019-08-25 04:23:55 +0000

    Added fonts

--- 2019-android-applications/ug-scheduler/presentation/src/main/java/dev/ugscheduler/core/UGScheduler.kt
+++ 2019-android-applications/ug-scheduler/presentation/src/main/java/dev/ugscheduler/core/UGScheduler.kt
@@ -2,11 +2,4 @@ package dev.ugscheduler.core
 
 import android.app.Application
 
-class UGScheduler  : Application() {
-
-    override fun onCreate() {
-        super.onCreate()
-
-        // todo: Initialise stuff here
-    }
-}
+class UGScheduler : Application()
