bcd63e56c6d9f409c1edf2917a5b55a3c1c5986e
Dennis Kwabena Bilson Jr <41457025+Quabynah-Codelabs@users.noreply.github.com>
2019-09-03 10:23:46 +0000

    Added github repo

--- 2019-android-applications/github-repo/app/src/main/java/io/codelabs/githubrepo/MainActivity.kt
+++ 2019-android-applications/github-repo/app/src/main/java/io/codelabs/githubrepo/MainActivity.kt
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2019. Designed & developed by Quabynah Codelabs(c). For the love of Android development.
+ */
+
 package io.codelabs.githubrepo
 
 import androidx.appcompat.app.AppCompatActivity
