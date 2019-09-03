6a808d70b074f043629e3c819ecc2b2be9d1be80
Dennis Kwabena Bilson Jr <41457025+Quabynah-Codelabs@users.noreply.github.com>
2019-09-03 05:13:43 +0000

    Updated avatar color in dark mode

--- 2019-android-applications/ug-scheduler-app/app/src/main/java/dev/ugscheduler/ui/courses/CourseDetailsFragment.kt
+++ 2019-android-applications/ug-scheduler-app/app/src/main/java/dev/ugscheduler/ui/courses/CourseDetailsFragment.kt
@@ -8,15 +8,17 @@ import android.view.ViewGroup
 import dev.ugscheduler.databinding.FragmentCourseDetailsBinding
 import dev.ugscheduler.shared.data.Course
 import dev.ugscheduler.shared.util.activityViewModelProvider
-import dev.ugscheduler.shared.util.debugger
+import dev.ugscheduler.shared.util.prefs.UserSharedPreferences
 import dev.ugscheduler.ui.settings.SettingsViewModel
 import dev.ugscheduler.ui.settings.SettingsViewModelFactory
 import dev.ugscheduler.util.MainNavigationFragment
+import org.koin.android.ext.android.inject
 
 
 class CourseDetailsFragment : MainNavigationFragment() {
     private lateinit var binding: FragmentCourseDetailsBinding
     private lateinit var userViewModel: SettingsViewModel
+    private val prefs by inject<UserSharedPreferences>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -33,13 +35,15 @@ class CourseDetailsFragment : MainNavigationFragment() {
         // Get course from bundle data
         val course = arguments?.get("extra_course") as? Course
         binding.course = course
-        debugger(course)
 
         // todo: Get facilitator information
 
         // todo: Get course sessions
 
         // todo: check if user is enrolled for course already
+        if (prefs.isLoggedIn) {
+            // todo: show user only info
+        }
     }
 
 
