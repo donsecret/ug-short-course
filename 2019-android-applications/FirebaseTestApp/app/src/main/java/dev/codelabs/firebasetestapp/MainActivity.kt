ea1284de50c57a15dc8e10000329d96c4c61af37
Dennis Kwabena Bilson Jr <41457025+Quabynah-Codelabs@users.noreply.github.com>
2019-10-11 17:29:34 +0000

    Main activity updated

--- 2019-android-applications/FirebaseTestApp/app/src/main/java/dev/codelabs/firebasetestapp/MainActivity.kt
+++ 2019-android-applications/FirebaseTestApp/app/src/main/java/dev/codelabs/firebasetestapp/MainActivity.kt
@@ -6,10 +6,19 @@ import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.SetOptions
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     private val auth: FirebaseAuth by lazy { FirebaseAuth.getInstance() }
+    private val firestore: FirebaseFirestore by lazy { FirebaseFirestore.getInstance() }
+    private val rtDatabase: FirebaseDatabase by lazy { FirebaseDatabase.getInstance() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -92,8 +101,93 @@ class MainActivity : AppCompatActivity() {
             sign_out_button.isEnabled = this
             login_button.isEnabled = !this
             create_account_button.isEnabled = !this
+
+            if (user != null) {
+                // Store info in database
+                storeInDatabase(user, true)
+                storeInDatabase(user, false)
+            }
+        }
+    }
+
+    private fun storeInDatabase(user: FirebaseUser, isFirestore: Boolean) {
+        if (isFirestore) {
+            // Store user information in Firebase Firestore
+            debugger("Storing in firestore")
+
+            firestore.collection(USER_REF).document(user.uid).set(AppUser(user.uid, user.email))
+                .addOnCompleteListener(this) {
+                    debugger("user stored")
+                }
+                .addOnFailureListener(this) {
+                    debugger("Unable to store user information")
+                }
+
+            // Check whether user data exists
+            firestore.runTransaction { transaction ->
+                val userDoc = transaction.get(firestore.collection(USER_REF).document(user.uid))
+
+                if (userDoc.exists()) {
+                    val appUser: AppUser? = userDoc.toObject(AppUser::class.java)
+                    debugger("Existing user => $appUser")
+                    // todo: store user in local database for future reference
+                } else {
+                    val appUser = AppUser(user.uid, user.email)
+                    transaction.set(
+                        firestore.collection(USER_REF).document(user.uid),
+                        appUser, SetOptions.merge()
+                    )
+                    debugger("New user => $appUser")
+                    // todo: store user in local database for future reference
+                }
+                null
+            }
+        } else {
+            // Store user information in Firebase Real-Time Database
+            debugger("Storing in real-time database")
+
+            val userRef = rtDatabase.reference.child(USER_REF).child(user.uid)
+            userRef.addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onCancelled(p0: DatabaseError) {
+                    debugger(p0.message)
+                }
+
+                override fun onDataChange(p0: DataSnapshot) {
+                    if (p0.exists()) {
+                        val appUser = p0.getValue(AppUser::class.java)
+                        debugger("RTDB Existing user => $appUser")
+                        // todo: store user in local database for future reference
+                    } else {
+                        // Create new user object
+                        val appUser = AppUser(user.uid, user.email)
+
+                        // Store in database
+                        userRef.setValue(appUser)
+                            .addOnCompleteListener(this@MainActivity) {
+                                if (it.isSuccessful) {
+                                    debugger("New user => $appUser")
+                                    // todo: store user in local database for future reference
+                                } else {
+                                    debugger(it.exception?.localizedMessage)
+                                }
+                            }
+                            .addOnFailureListener(this@MainActivity) {
+                                debugger(it.localizedMessage)
+                            }
+                    }
+                }
+            })
         }
     }
 }
 
 fun debugger(msg: Any?) = println("FirebaseTestApp =>  ${msg.toString()}")
+
+private const val USER_REF = "users"
+
+// App user's data model
+data class AppUser(
+    val id: String,
+    val email: String?,
+    val timestamp: Date = Date(System.currentTimeMillis())
+)
