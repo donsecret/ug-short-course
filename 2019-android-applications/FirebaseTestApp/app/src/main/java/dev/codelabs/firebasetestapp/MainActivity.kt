fb9b83a226ac6cd2a5911f0258cb3ada73a0e51a
Dennis Kwabena Bilson Jr <41457025+Quabynah-Codelabs@users.noreply.github.com>
2019-10-11 16:21:48 +0000

    Added firebase

--- 2019-android-applications/FirebaseTestApp/app/src/main/java/dev/codelabs/firebasetestapp/MainActivity.kt
+++ 2019-android-applications/FirebaseTestApp/app/src/main/java/dev/codelabs/firebasetestapp/MainActivity.kt
@@ -1,12 +1,34 @@
 package dev.codelabs.firebasetestapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class MainActivity : AppCompatActivity() {
+    private val auth: FirebaseAuth by lazy { FirebaseAuth.getInstance() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Firebase application initialization
+        FirebaseApp.initializeApp(this).apply {
+            debugger("Firebase initialized as : ${this?.name}")
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+        updateUI(currentUser)
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+        //
+        debugger("User is: $user")
     }
 }
+
+fun debugger(msg: Any?) = println("FirebaseTestApp =>  ${msg.toString()}")
