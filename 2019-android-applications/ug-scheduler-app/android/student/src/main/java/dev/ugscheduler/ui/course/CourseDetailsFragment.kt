7f0afad5475a3205bfd76b424f152973216aecd0
Dennis Kwabena Bilson Jr <41457025+Quabynah-Codelabs@users.noreply.github.com>
2019-10-16 16:31:15 +0000

    Update CourseDetailsFragment.kt

--- 2019-android-applications/ug-scheduler-app/android/student/src/main/java/dev/ugscheduler/ui/course/CourseDetailsFragment.kt
+++ 2019-android-applications/ug-scheduler-app/android/student/src/main/java/dev/ugscheduler/ui/course/CourseDetailsFragment.kt
@@ -4,17 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import dev.ugscheduler.R
+import dev.ugscheduler.databinding.FragmentCourseDetailsBinding
 import dev.ugscheduler.util.MainNavigationFragment
 
 class CourseDetailsFragment : MainNavigationFragment() {
+    private lateinit var binding: FragmentCourseDetailsBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_course_details, container, false)
+        binding = FragmentCourseDetailsBinding.inflate(inflater)
+        return binding.root
     }
-
 }
