405bb4b9d7580020c6da6db9aa46a7acf167b8fe
Dennis Kwabena Bilson Jr <41457025+Quabynah-Codelabs@users.noreply.github.com>
2019-10-18 15:39:57 +0000

    Update EnrolFragment.kt

--- 2019-android-applications/ug-scheduler-app/android/student/src/main/java/dev/ugscheduler/ui/enrol/EnrolFragment.kt
+++ 2019-android-applications/ug-scheduler-app/android/student/src/main/java/dev/ugscheduler/ui/enrol/EnrolFragment.kt
@@ -6,7 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
-import dev.ugscheduler.BuildConfig
+import dev.ugscheduler.BuildConfig.FLAVOR
 import dev.ugscheduler.databinding.FragmentEnrolBinding
 import dev.ugscheduler.shared.data.Course
 import dev.ugscheduler.shared.data.Student
@@ -37,7 +37,7 @@ class EnrolFragment : MainNavigationFragment() {
                 Enrolment(student.id, course.id, Session.WEEKEND.sessionName())
             debugger(enrolment)*/
 
-            //debugger(FLAVOR)
+            debugger(FLAVOR)
         } else {
             toast("Cannot setup enrolment. An error occurred")
             findNavController().popBackStack()
