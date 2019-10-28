de7cc609f30a657f07acc787dd06b5b6f7ce058f
Dennis Kwabena Bilson Jr <41457025+Quabynah-Codelabs@users.noreply.github.com>
2019-10-28 08:12:32 +0000

    Updated enrolment fragment

--- 2019-android-applications/ug-scheduler-app/student/src/main/java/dev/ugscheduler/ui/enrol/EnrolFragment.kt
+++ 2019-android-applications/ug-scheduler-app/student/src/main/java/dev/ugscheduler/ui/enrol/EnrolFragment.kt
@@ -4,46 +4,50 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
-import dev.ugscheduler.BuildConfig.FLAVOR
-import dev.ugscheduler.databinding.FragmentEnrolBinding
 import dev.ugscheduler.shared.data.Course
-import dev.ugscheduler.shared.data.Enrolment
 import dev.ugscheduler.shared.data.Student
-import dev.ugscheduler.shared.data.sessionName
-import dev.ugscheduler.shared.util.debugger
+import dev.ugscheduler.shared.util.activityViewModelProvider
 import dev.ugscheduler.shared.util.toast
+import dev.ugscheduler.shared.viewmodel.AppViewModel
+import dev.ugscheduler.shared.viewmodel.AppViewModelFactory
 import dev.ugscheduler.util.MainNavigationFragment
-import java.util.*
+import org.koin.android.ext.android.get
 
 class EnrolFragment : MainNavigationFragment() {
-    private lateinit var binding: FragmentEnrolBinding
-    private lateinit var viewModel: EnrolViewModel
+//    private lateinit var binding: FragmentEnrol
+    private val viewModel by lazy {
+        activityViewModelProvider<AppViewModel>(
+            AppViewModelFactory(get())
+        )
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentEnrolBinding.inflate(inflater)
-        return binding.root
+        /*binding = FragmentEnrolBinding.inflate(inflater)
+        return binding.root*/
+        return null
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(EnrolViewModel::class.java)
 
         val course = arguments?.get("extra_course") as? Course
         val student = arguments?.get("extra_student") as? Student
         if (course != null && student != null) {
-            val enrolment =
-                Enrolment(student.id, course.id, Enrolment.Session.WEEKEND.sessionName())
-            debugger(enrolment)
-            debugger(FLAVOR)
+            bindUI(course, student)
         } else {
             toast("Cannot setup enrolment. An error occurred")
             findNavController().popBackStack()
         }
     }
+
+    // Bind details for the course
+    private fun bindUI(course: Course, student: Student) {
+        // todo: enable enrolment of student for course
+    }
+
 
 }
--- 2019-android-applications/ug-scheduler-app/student/src/main/java/dev/ugscheduler/ui/enrol/EnrolViewModel.kt
+++
@@ -1,7 +0,0 @@
-package dev.ugscheduler.ui.enrol
-
-import androidx.lifecycle.ViewModel
-
-class EnrolViewModel : ViewModel() {
-
-}
